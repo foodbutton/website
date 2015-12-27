439dead02bf4d3a9c380ac71690f49d7f9268905
Michael Frohberg <mfrohberg@2u.com>
2015-12-26 22:20:54 -0500

    bootstrapped with angular2 starter kit

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {Component} from 'angular2/core';
+import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Http} from 'angular2/http';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+
+@Component({
+  selector: 'app',
+  providers: [ ...FORM_PROVIDERS],
+  directives: [ ...ROUTER_DIRECTIVES],
+  // We need to tell Angular's compiler which custom pipes are in our template.
+  pipes: [],
+  styles: [],
+  template: `
+    <header>
+      <h1 class="title">FoodButton {{+ '!'}}</h1>
+    </header>
+
+    <main>
+    </main>
+  `
+})
+
+
+export class App {}
