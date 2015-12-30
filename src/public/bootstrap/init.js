b0572f246a735fbc6e5d98d9ed925e965b131e8e
Michael Frohberg <mfrohberg@2u.com>
2015-12-30 06:49:12 -0500

    add ng2-material dependencies

---
+++ src/public/bootstrap/init.js
@@ -0,0 +1,24 @@
+//  var Stripe = window.Stripe;
+//  var FB = window.FB;
+//  var AWS = window.AWS;
+
+//  Stripe.setPublishableKey('pk_test_wUzLERCtFlMGQt8iL9Bj8DlB');
+
+//  window.fbAsyncInit = function() {
+//     FB.init({
+//         appId      : '1238491436164644',
+//         cookie     : true,  // enable cookies to allow the server to access
+//                             // the session
+//         xfbml      : true,  // parse social plugins on this page
+//         version    : 'v2.5' // use version 2.2
+//      });
+//  };
+
+// // Load the FB SDK asynchronously
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = "//connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
